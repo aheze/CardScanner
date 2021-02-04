cbefcaf814795153ccffe2c35cc449061495608f
Zheng <ahzzheng@gmail.com>
2021-02-04 13:04:45 -0800

    Initial Commit

---
+++ CardScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardScanner
+//
+//  Created by Zheng on 2/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
